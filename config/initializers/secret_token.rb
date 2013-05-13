c18d5f2609215ca83fd75299222e3eb3f3909936
unayung <Unayung@gmail.com>
2013-05-13 21:22:00 +0800

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RhodesPc2::Application.config.secret_token = 'bcdd72bc432fd2cd8e36f493fb69c470e6319a4e7350373061bd5a62012a46af14943f6b4d2ab0d4cfce1f4a10ae69f88c06b9cf7a0b68fae0015a7c8fa68123'
