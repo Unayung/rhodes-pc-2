75c7f35c4c2294ef76b4e6a148b6cdc033e5d725
unayung <Unayung@gmail.com>
2013-11-01 22:43:14 +0800

    event edit

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,10 +9,14 @@ class Event < ActiveRecord::Base
   def to_param
     url # or whatever you set :url_attribute to
   end
-  
+
   def self.is_previous(month)
     month_begin = "#{Date.today.year}-#{month}-1".to_date
     month_end = month_begin.end_of_month
     where(:will_publish_at => month_begin..month_end)
+  end
+
+  def file_name
+    event_file.original_filename
   end
 end
