6b6bd92013024d489389ad4a13e0a5c2753e9852
Unayung Chen <unayung@gmail.com>
2013-12-02 16:34:01 +0800

    fix month

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < HighVoltage::PagesController
     if params[:month].present?
       @month = params[:month]
     else
-      @month = Date.today.month
+      @month = DateTime.now.month
     end
     @people = Person.all
     @news = News.is_previous(@month).is_published
@@ -13,11 +13,11 @@ class PagesController < HighVoltage::PagesController
     @year = Date.today.year
     @display_month = "#{@year}-#{@month}-1".to_date.strftime("%B")
 
-    @news_has_next = News.is_previous(@month.to_i+1).is_published.any?
-    @news_has_previous = News.is_previous(@month.to_i-1).is_published.any?
+    @news_has_next = News.is_previous((DateTime.now + 1.month).month).is_published.any?
+    @news_has_previous = News.is_previous((DateTime.now - 1.month).month).is_published.any?
 
-    @events_has_next = Event.is_previous(@month.to_i+1).is_published.any?
-    @events_has_previous = Event.is_previous(@month.to_i-1).is_published.any?
+    @events_has_next = Event.is_previous((DateTime.now + 1.month).month).is_published.any?
+    @events_has_previous = Event.is_previous((DateTime.now - 1.month).month).is_published.any?
   end
 
   protected
