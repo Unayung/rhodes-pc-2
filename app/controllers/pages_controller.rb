0a7c4cbc2e24ff3ce8201e416d5f915485ebc441
Unayung Chen <unayung@gmail.com>
2013-12-02 17:19:46 +0800

    temporary remove news and update next and previous

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,20 +4,33 @@ class PagesController < HighVoltage::PagesController
   def landing
     if params[:month].present?
       @month = params[:month]
+      if @month.to_i == 12
+        @next_month = 1
+        @previous_month = 11
+      elsif @month.to_i == 1
+        @previous_month = 12
+        @next_month = 2
+      else
+        @next_month  = @month.to_i + 1
+        @previous_month = @month.to_i - 1
+      end
     else
       @month = DateTime.now.month
+      @next_month = (DateTime.now + 1.month).month
+      @previous_month = (DateTime.now - 1.month).month
     end
     @people = Person.all
     @news = News.is_previous(@month).is_published
     @events = Event.is_previous(@month).is_published
     @year = Date.today.year
-    @display_month = "#{@year}-#{@month}-1".to_date.strftime("%B")
+    @display_date = "#{Date.today.strftime("%B")} #{Date.today.strftime("%Y")}"
+    # @display_month = "#{@year}-#{@month}-1".to_date.strftime("%B")
 
-    @news_has_next = News.is_previous((DateTime.now + 1.month).month).is_published.any?
-    @news_has_previous = News.is_previous((DateTime.now - 1.month).month).is_published.any?
+    # @news_has_next = News.is_previous(@next_month).is_published.any?
+    # @news_has_previous = News.is_previous(@previous_month).is_published.any?
 
-    @events_has_next = Event.is_previous((DateTime.now + 1.month).month).is_published.any?
-    @events_has_previous = Event.is_previous((DateTime.now - 1.month).month).is_published.any?
+    # @events_has_next = Event.is_previous(@next_month).is_published.any?
+    # @events_has_previous = Event.is_previous(@previous_month).is_published.any?
   end
 
   protected
