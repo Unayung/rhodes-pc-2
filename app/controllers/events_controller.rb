f2c4dfcbf5697606ceef15e32d226304bd224457
Unayung Chen <unayung@gmail.com>
2013-12-04 02:57:56 +0800

    add news and events list

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,9 @@
 class EventsController < ApplicationController
+  layout "bio"
+  
+  def index
+    @events = Event.paginate(:page => params[:page], :per_page => 10, :order => "created_at DESC")
+  end
 
   def show
     @file = Event.find_by_url(params[:id]).event_file.path
--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -5,4 +5,7 @@ class NewsController < ApplicationController
     @news = News.find_by_url(params[:id])
   end
 
+  def index
+    @news = News.paginate(:page => params[:page], :per_page => 10, :order => "created_at DESC")
+  end
 end
